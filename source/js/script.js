4063e6753e982521e35fc715a30a5f125a476c24
Anna Kavarnukaeva <a.kavarnukaeva@gmail.com>
2018-11-05 19:26:16 +0300

    Завершает вёрстку модального окна и выполняет реализацию его открытия/закрытия на js

---
+++ source/js/script.js
@@ -0,0 +1,41 @@
+'use strict';
+
+(function() {
+  var ESCAPE_KEY = 27;
+  // var navHeader = document.querySelector('.page-header__navigation');
+  // var navToggle = document.querySelector('.page-header__button');
+  var orderButton = document.querySelector(".weeks-bestseller__order-button");
+  var popup = document.querySelector(".modal");
+
+ // navHeader.classList.remove('page-header__navigation--nojs');
+ //
+ // navToggle.addEventListener('click', function() {
+ //   if (navHeader.classList.contains('page-header__navigation--closed')) {
+ //     navHeader.classList.remove('page-header__navigation--closed');
+ //     navHeader.classList.add('page-header__navigation--opened');
+ //   } else {
+ //     navHeader.classList.add('page-header__navigation--closed');
+ //     navHeader.classList.remove('page-header__navigation--opened');
+ //   }
+ // });
+
+ orderButton.addEventListener("click", function (evt) {
+   evt.preventDefault();
+   popup.classList.add("modal-show");
+ });
+
+  document.addEventListener("click", function (evt) {
+    if (evt.target !== orderButton && evt.target !== popup && evt.target.parentNode !== popup && popup.classList.contains("modal-show")) {
+      popup.classList.remove("modal-show");
+    }
+  });
+
+  window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === ESCAPE_KEY) {
+      evt.preventDefault();
+      if (popup.classList.contains("modal-show")) {
+        popup.classList.remove("modal-show");
+      }
+    }
+  });
+})();
