e89354caa8bb44812157225c24948a4f8913886f
Anna Kavarnukaeva <a.kavarnukaeva@gmail.com>
2018-11-06 03:58:04 +0300

    Вносит правки к заданию и добавляет состояния для элементов

--- source/js/script.js
+++ source/js/script.js
@@ -7,22 +7,22 @@
   var orderButton = document.querySelector(".weeks-bestseller__order-button");
   var popup = document.querySelector(".modal");
 
- // navHeader.classList.remove('page-header__navigation--nojs');
- //
- // navToggle.addEventListener('click', function() {
- //   if (navHeader.classList.contains('page-header__navigation--closed')) {
- //     navHeader.classList.remove('page-header__navigation--closed');
- //     navHeader.classList.add('page-header__navigation--opened');
- //   } else {
- //     navHeader.classList.add('page-header__navigation--closed');
- //     navHeader.classList.remove('page-header__navigation--opened');
- //   }
- // });
+// navHeader.classList.remove('page-header__navigation--nojs');
+//
+// navToggle.addEventListener('click', function() {
+//  if (navHeader.classList.contains('page-header__navigation--closed')) {
+//    navHeader.classList.remove('page-header__navigation--closed');
+//    navHeader.classList.add('page-header__navigation--opened');
+//  } else {
+//    navHeader.classList.add('page-header__navigation--closed');
+//    navHeader.classList.remove('page-header__navigation--opened');
+//  }
+// });
 
- orderButton.addEventListener("click", function (evt) {
-   evt.preventDefault();
-   popup.classList.add("modal-show");
- });
+  orderButton.addEventListener("click", function (evt) {
+      evt.preventDefault();
+      popup.classList.add("modal-show");
+  });
 
   document.addEventListener("click", function (evt) {
     if (evt.target !== orderButton && evt.target !== popup && evt.target.parentNode !== popup && popup.classList.contains("modal-show")) {
