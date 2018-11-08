07ccb2c87999b623ac38294861753282b7c09618
Anna Kavarnukaeva <a.kavarnukaeva@gmail.com>
2018-11-08 16:02:21 +0300

    Вносит правки, добавляет svg-спрайт, подключает изображения в формате webp

--- source/js/script.js
+++ source/js/script.js
@@ -2,32 +2,42 @@
 
 (function() {
   var ESCAPE_KEY = 27;
-  // var navHeader = document.querySelector('.page-header__navigation');
-  // var navToggle = document.querySelector('.page-header__button');
+
+  var cartIcon = document.querySelectorAll(".catalog__cart-link");
   var orderButton = document.querySelector(".weeks-bestseller__order-button");
   var popup = document.querySelector(".modal");
+  var menuButton = document.querySelector(".page-header__button");
+  var header = document.querySelector(".page-header");
+  var navigation = document.querySelectorAll(".page-header__navigation");
 
-// navHeader.classList.remove('page-header__navigation--nojs');
-//
-// navToggle.addEventListener('click', function() {
-//  if (navHeader.classList.contains('page-header__navigation--closed')) {
-//    navHeader.classList.remove('page-header__navigation--closed');
-//    navHeader.classList.add('page-header__navigation--opened');
-//  } else {
-//    navHeader.classList.add('page-header__navigation--closed');
-//    navHeader.classList.remove('page-header__navigation--opened');
-//  }
-// });
-
-  orderButton.addEventListener("click", function (evt) {
-      evt.preventDefault();
-      popup.classList.add("modal-show");
+  window.addEventListener('DOMContentLoaded', function () {
+    header.classList.remove('page-header--no-js');
   });
 
-  document.addEventListener("click", function (evt) {
-    if (evt.target !== orderButton && evt.target !== popup && evt.target.parentNode !== popup && popup.classList.contains("modal-show")) {
-      popup.classList.remove("modal-show");
-    }
+  if (orderButton) {
+    orderButton.addEventListener("click", function (evt) {
+        evt.preventDefault();
+        popup.classList.add("modal-show");
+    });
+  }
+
+  if (popup) {
+    document.addEventListener("click", function (evt) {
+      if (!popup.contains(evt.target) && evt.target !== orderButton
+        && popup.classList.contains("modal-show")
+        && !evt.target.classList.contains('catalog__cart-link')
+        && !evt.target.classList.contains('catalog__cart-icon')) {
+          console.log('meow');
+          popup.classList.remove("modal-show");
+      }
+    });
+  }
+
+  [].forEach.call(cartIcon, function(el) {
+    el.addEventListener("click", function (evt) {
+      evt.preventDefault();
+      popup.classList.add("modal-show");
+    });
   });
 
   window.addEventListener("keydown", function (evt) {
@@ -37,5 +47,13 @@
         popup.classList.remove("modal-show");
       }
     }
+  });
+
+  menuButton.addEventListener('click', function() {
+    menuButton.classList.toggle('page-header__button--close');
+
+    [].forEach.call(navigation, function(el) {
+      el.classList.toggle('page-header__navigation--opened');
+    });
   });
 })();
